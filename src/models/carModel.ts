20d0b248cb8cc9d7709c8aa5a7255118d1c8dd3b
PauloVFC94 <paulovictorfcosta@gmail.com>
2022-10-24 23:34:03 +0000

    Feat: add Model Mongo e Car

---
+++ src/models/carModel.ts
@@ -0,0 +1,20 @@
+import { model as mongooseCreateModel , Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const carsMongoSchema = new Schema<ICar>({
+  model: String,
+  year: Number,
+  color: String,
+  buyValue: Number,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+class Car extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('Car', carsMongoSchema)) {
+    super(model);
+  }
+}
+
+export default Car;
