13b9447ef436128b14c64b7187505ad5e760e544
PauloVFC94 <paulovictorfcosta@gmail.com>
2022-10-25 00:21:38 +0000

    Feat: Implementa a rota Post /cars

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import route from './routes/carRoute';
+import errorHandler from './middlewares/error';
 
 const app = express();
+
+app.use(express.json());
+app.use(route);
+app.use(errorHandler);
 
 export default app;
